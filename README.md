9560b77e630beef32cafad7588799f4671327010
Kaushik Paul <kaushikpaul50@gmail.com>
2021-04-10 15:19:06 -0400

    Added express, sessions, some login func

--- README.md
+++ README.md
@@ -5,4 +5,8 @@ Kaushik Paul - 101158930
 
 
 Instructions
+-> Install dependencies with "npm install"
 -> Run "node .\server.js" in console and go to http://localhost:3000/
+
+
+RESTful design document & Data Model outline found Documentation folder within the Zip and also in the CULEARN Check In submission page
--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const http = require('http');
 const pug = require('pug');
+const express = require('express');
+let app = express();
+app.set("view engine", "pug");
+app.use(express.json());
+app.use(express.urlencoded());
+const session = require('express-session');
+app.use(session({
+    secret: 'some secret key here',
+    viewedMovies: [],
+    resave: true,
+    saveUninitialized: true
+
+}));
+
 
 //Set up the required data
 let movieData = require("./movie-data-10.json");
+const e = require('express');
 let movies = {}; //Stores all of the movies, key=id
 let returnedMovie = {};
 let nextID=0;
@@ -24,116 +39,114 @@ function returnMovie(ID){
     });
 }
 //Initialize server
-const server = http.createServer(function (request, response) {
-	//console.log(request.method);
-	if(request.method === "GET"){
-        let para = request.url;
-		para = para.split("/")[2];
-		
-		if(request.url === "/" || request.url === "/index.pug"){
-            //console.log(movies);
-			let data = pug.renderFile("index.pug", {movies: movies});
-			//console.log(movies);
-			response.statusCode = 200;
-			response.end(data);
-			return;
-		}
-		if(request.url === "/movie/"+para){
-            movieTitle = decodeURI(para);
-            returnMovie(para);
-			let data = pug.renderFile("movie.pug",{movie:returnedMovie[0]});
-			response.statusCode = 200;
-			response.end(data);
-			return;		
-		}
-        if(request.url === "/ownProfile"){
-            let data = pug.renderFile("exampleProfile.pug");
-			response.statusCode = 200;
-			response.end(data);
-			return;	
-        }
-        if(request.url === "/profile"){
-            let data = pug.renderFile("exampleOtherProfile.pug");
-			response.statusCode = 200;
-			response.end(data);
-			return;	
-        }
-        if(request.url === "/person"){
-            let data = pug.renderFile("examplePerson.pug");
-			response.statusCode = 200;
-			response.end(data);
-			return;
-        }
-        if (request.url === "/review"){
-            let data = pug.renderFile("exampleReview.pug");
-			response.statusCode = 200;
-			response.end(data);
-			return;
-        }
 
-        if(request.url === "/search"){
-            let data = pug.renderFile("search.pug");
-			response.statusCode = 200;
-			response.end(data);
-			return;
-        }
-        if(request.url === "/creation"){
-            let data = pug.renderFile("creation.pug");
-			response.statusCode = 200;
-			response.end(data);
-			return;
-        }
-		if (request.url === "/contribute") {
-			let data = pug.renderFile("contribute.pug");
-			response.statusCode = 200;
-			response.end(data);
-			return;
-		}
+app.get("/", async(req,res,next)=>{
+	let data = pug.renderFile("index.pug", {movies: movies});
+	res.status=200;
+	res.send(data);
+	console.log(req.session.viewedMovies);
+});
 
+app.get("/movie/:movieID", async(req,res,next)=>{
+	if(req.session.viewedMovies){
+		req.session.viewedMovies.push(req.params.movieID);
 	}
-    if(request.method === "POST"){
-        if(request.url==="/searchresults?"){
-            let searchResults = movies;
-            let data = pug.renderFile("resultsExample.pug",{movies:searchResults});
-			response.statusCode = 200;
-			response.end(data);
-			return;
-        }
-        if(request.url==="/createAccount?"  || request.url==="/login?" ){
-			response.statusCode = 200;
-			response.end("Created/Logged In Requested!");
-			return;
-        }
-		if (request.url==="/addActor?") {
-			response.statusCode = 200;
-			response.end("Actor Added");
-			return;
-		}
-		if (request.url==="/addMovie?") {
-			response.statusCode = 200;
-			response.end("Movie Added");
-			return;
-		}
-		if (request.url === "/addReview/basic?"){
-			response.statusCode = 200;
-			response.end("Review Added");
-			return;
-		}
-		if (request.url === "/addReview/full?"){
-			response.statusCode = 200;
-			response.end("Review Added");
-			return;
-		}
-    }
-
 	else{
-		response.statusCode = 404;
-		response.write("Unknwn resource.");
-		response.end();
+		req.session.viewedMovies = [req.params.movieID];
 	}
+	
+	returnMovie(req.params.movieID);
+	let data = pug.renderFile("movie.pug",{movie:returnedMovie[0]});
+	res.status=200;
+	res.send(data);
+});
+
+app.get("/ownProfile", async(req,res,next)=>{
+	//Set up to send a user object, which would be the logged in one
+	let data = pug.renderFile("exampleProfile.pug");
+	res.statusCode = 200;
+	res.end(data);
+});
+
+app.get("/profile/:profileID",async(req,res,next)=>{
+	let data = pug.renderFile("exampleOtherProfile.pug");
+	res.statusCode = 200;
+	res.end(data);
+});
+
+app.get("/person/:personID", async(req,res,next)=>{
+	let data = pug.renderFile("examplePerson.pug");
+	res.statusCode = 200;
+	res.end(data);
+});
+
+app.get("/review/:reviewID", async(req,res,next)=>{
+	let data = pug.renderFile("exampleReview.pug");
+	res.statusCode = 200;
+	res.end(data);
+});
+
+app.get("/search", async(req,res,next)=>{
+	let data = pug.renderFile("search.pug");
+	res.statusCode = 200;
+	res.end(data);
 	
 });
 
-//Start server
-server.listen(3000);
+app.get("/creation", async(req,res,next)=>{
+	let data = pug.renderFile("creation.pug");
+	res.statusCode = 200;
+	res.end(data);
+	
+});
+app.get("/contribute", async(req,res,next)=>{
+	let data = pug.renderFile("contribute.pug");
+	res.statusCode = 200;
+	res.end(data);
+	
+});
+
+app.post("/searchresults?",async (req,res,next)=>{
+	let searchResults = movies;
+	let data = pug.renderFile("resultsExample.pug",{movies:searchResults});
+	res.statusCode = 200;
+	res.end(data);
+});
+
+app.post("/createAccount",async(req,res,next)=>{
+	let username = req.body.username;
+	let password = req.body.password; 
+	console.log(username);
+	console.log(password);
+	
+	res.statusCode = 200;
+	res.end("Creation Requested!");
+});
+
+app.post("/login?",async(req,res,next)=>{
+	res.statusCode = 200;
+	res.end("Login Requested!");
+});
+
+app.post("/addActor?",async(req,res,next)=>{
+	res.statusCode = 200;
+	res.end("Actor addition Requested!");
+});
+
+app.post("/addMovie?",async(req,res,next)=>{
+	res.statusCode = 200;
+	res.end("Movie addition Requested!");
+});
+
+app.post("/addReview/basic?",async(req,res,next)=>{
+	res.statusCode = 200;
+	res.end("Review basic addition Requested!");
+});
+
+app.post("/addReview/full?",async(req,res,next)=>{
+	res.statusCode = 200;
+	res.end("Review full addition Requested!");
+});
+
+app.listen(3000);
 console.log("Server listening at http://localhost:3000");
